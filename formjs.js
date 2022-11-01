5a83710d33e594db1c95786f6c063a07288fa155
SamSouto <116651321+SamSouto@users.noreply.github.com>
2022-11-01 11:22:32 -0300

    valida_form

---
+++ formjs.js
@@ -0,0 +1,12 @@
+function validaFormulario(){
+    let pront = document.getElementById("Prontuario").value
+    let senha = document.getElementById("Senha").value
+    senha = parseInt(senha);
+    }
+
+    function validaFormADM(){
+    let email = document.getElementById("Email").value
+    email = parseInt(email);
+    let senha = document.getElementById("Senha").value
+    senha = parseInt(senha);
+    }
